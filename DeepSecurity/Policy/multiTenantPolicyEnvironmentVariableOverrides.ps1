23d9290e024dc270f69883bbedc3afd8f157dff8
jeffcaten <jeffcaten@gmail.com>
2022-08-04 13:29:42 -0500

    Change powershell version to 5.0.  Remove powershell 7.0 features.

--- DeepSecurity/Policy/multiTenantPolicyEnvironmentVariableOverrides.ps1
+++ DeepSecurity/Policy/multiTenantPolicyEnvironmentVariableOverrides.ps1
@@ -49,7 +49,7 @@ test02, Delete temp API Key
 This script should clean up the ApiKeys that it creates.  If the script can't delete the modifyPolicy ApiKey for some reason an adminitrator will need to clean up the left over ApiKey from the effected tenants.
 If this script fails to delete the API key, the API key is set to expire about 30 minutes after it was created.
 #>
-#requires -version 7.0
+#requires -version 5.0
 
 param (
     [Parameter(Mandatory=$true, HelpMessage="FQDN and port for Deep Security Manager; ex dsm.example.com:443--")][string]$manager,
@@ -62,7 +62,7 @@ param (
 $ProgressPreference = 'SilentlyContinue' 
 
 # Set Cert verification and TLS version to 1.2.
-#[System.Net.ServicePointManager]::ServerCertificateValidationCallback={$false}
+[System.Net.ServicePointManager]::ServerCertificateValidationCallback={$false}
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # Headers to use for all Api queries to T0
@@ -90,7 +90,7 @@ function tenatSearchFunction {
     }
     $tenantSearchBody = $tenantSearchHash | ConvertTo-Json
     
-    $tenantSearchResults = Invoke-WebRequest -Uri $tenantSearchURL -Method Post -ContentType "application/json" -Headers $headers -Body $tenantSearchBody -SkipCertificateCheck  | ConvertFrom-Json
+    $tenantSearchResults = Invoke-WebRequest -Uri $tenantSearchURL -Method Post -ContentType "application/json" -Headers $headers -Body $tenantSearchBody   | ConvertFrom-Json
 
     return $tenantSearchResults
 }
@@ -116,7 +116,7 @@ function createTenantApiKeyFunction {
     $createTenantApiKeyBody = $createTenantApiKeyHash | ConvertTo-Json
     
     try {
-        $createTenantApiKeyResults = Invoke-WebRequest -Uri $createTenantApiKeyURL -Method Post -ContentType "application/json" -Headers $headers -Body $createTenantApiKeyBody -SkipCertificateCheck  | ConvertFrom-Json
+        $createTenantApiKeyResults = Invoke-WebRequest -Uri $createTenantApiKeyURL -Method Post -ContentType "application/json" -Headers $headers -Body $createTenantApiKeyBody   | ConvertFrom-Json
     }
     catch {
         $tenantApiKeyCreateStatus = "Failed"
@@ -156,7 +156,7 @@ function deleteTenantApiKey {
 
     $deleteTenantApiKeyURL = "https://$manager/api/apikeys/$apiKeyID"
     try {
-        $deleteTenantApiKeyResults = Invoke-WebRequest -Uri $deleteTenantApiKeyURL -Method DELETE -ContentType "application/json" -Headers $deleteTenantApiKeyheaders -SkipCertificateCheck
+        $deleteTenantApiKeyResults = Invoke-WebRequest -Uri $deleteTenantApiKeyURL -Method DELETE -ContentType "application/json" -Headers $deleteTenantApiKeyheaders 
     }
     catch {
         $deleteTenantApiKeyStatus = "Failed"
@@ -198,7 +198,7 @@ function EnvironmentVariableOverridesLookupFunction{
     }
     $policySearchBody = $policySearchHash | ConvertTo-Json
     
-    $policySearchResults = Invoke-WebRequest -Uri $policySearchURL -Method Post -ContentType "application/json" -Headers $headers -Body $policySearchBody  -SkipCertificateCheck | ConvertFrom-Json
+    $policySearchResults = Invoke-WebRequest -Uri $policySearchURL -Method Post -ContentType "application/json" -Headers $headers -Body $policySearchBody   | ConvertFrom-Json
 
     return $policySearchResults
 }
@@ -227,7 +227,7 @@ function policySearchFunction {
     }
     $policySearchBody = $policySearchHash | ConvertTo-Json
     
-    $policySearchResults = Invoke-WebRequest -Uri $policySearchUrl -Method Post -ContentType "application/json" -Headers $policySearchFunctionHeaders -Body $policySearchBody -SkipCertificateCheck  | ConvertFrom-Json   
+    $policySearchResults = Invoke-WebRequest -Uri $policySearchUrl -Method Post -ContentType "application/json" -Headers $policySearchFunctionHeaders -Body $policySearchBody   | ConvertFrom-Json   
 
     return $policySearchResults
 
@@ -257,7 +257,7 @@ function policyModifyFunction {
     }
     $policyModifyBody = $policyModifyHash | ConvertTo-Json
     
-    $policyModifyResults = Invoke-WebRequest -Uri $policyModifyUrl -Method Post -ContentType "application/json" -Headers $policyModifyFunctionHeaders -Body $policyModifyBody -SkipCertificateCheck | ConvertFrom-Json 
+    $policyModifyResults = Invoke-WebRequest -Uri $policyModifyUrl -Method Post -ContentType "application/json" -Headers $policyModifyFunctionHeaders -Body $policyModifyBody  | ConvertFrom-Json 
     return $policyModifyResults
 }
 
